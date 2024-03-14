252352ccf7abb628f314bb74c25b7ffe7925c9b9
Ana Prado <153234400+anapprado@users.noreply.github.com>
2024-03-13 22:45:15 -0500

    innitial commit

---
+++ Expenses/src/components/ExpenseList.tsx
@@ -0,0 +1,55 @@
+import "bootstrap/dist/css/bootstrap.css"
+
+interface Expense {
+    id: number;
+    description:string;
+    amount: number;
+    category: string;
+ }
+
+interface Props {
+    expenses: Expense []
+    onDelete: (id: number) => void
+}
+
+const ExpenseList = ({expenses, onDelete }: Props) => {
+  return (
+    <div>
+        <table className="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Ammount</th>
+                    <th>Category</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {expenses.map( expense => <tr key={expense.id}>
+                    <td>{expense.description}</td>
+                    <td>{expense.amount}</td>
+                    <td>{expense.category}</td>
+                    <td>
+                        <button 
+                        className="btn btn-outline-danger"
+                        onClick={() =>onDelete (expense.id)}
+                        >
+                            Delete
+                        </button>
+                    </td>
+
+                </tr>)}
+            </tbody>
+            <tfoot>
+                <tr>Total</tr>
+                <tr>${expenses.reduce((acc, expense)=> expense.amount + acc, 0).toFixed(2)}
+                </tr>
+                <tr></tr>
+                <tr></tr>
+            </tfoot>
+        </table>
+    </div>
+  )
+}
+
+export default ExpenseList
