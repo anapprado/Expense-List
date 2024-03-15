e4b0ba9588ea36a937838e1da634c37cb5ba31a7
Ana Prado <153234400+anapprado@users.noreply.github.com>
2024-03-14 19:04:29 -0500

    updtate

--- Expenses/src/App.tsx
+++ Expenses/src/App.tsx
@@ -2,10 +2,13 @@ import { useState } from "react";
 import ExpenseList from "./components/ExpenseList"
 import "bootstrap/dist/css/bootstrap.css"
 import ExpenseFilter from "./components/ExpenseFilter";
+import ExpenseForm from "./components/ExpenseForm";
 
+export const categories = ["Groceries", "Utilities", "Entertainment"]as const;
 
 
 function App() {
+ const [selectedCategory, setSelectedCategory] = useState ("");
  const [expenses, setExpenses]= useState( [ 
   { id: 1, description: "fruits", amount: 10, category: "Groceries" },
   { id: 2, description: "fruits", amount: 20, category: "Utilities" },
@@ -13,13 +16,21 @@ function App() {
   { id: 4, description: "fruits", amount: 40, category: "Entertainment" },
   { id: 5, description: "fruits", amount: 50, category: "Entertainment" },
 ]);
-  
+
+const visibleExpenses = selectedCategory ? expenses.filter (e => e.category === selectedCategory) : expenses;
 
   return (
+    
     <div>
-      <ExpenseFilter onSelectCategory={category => console.log(category)} />
+      <div className="mb-5">
+        <ExpenseForm />
+        
+        </div>
+      <div className="mb-3">
+      <ExpenseFilter onSelectCategory={(category) => setSelectedCategory (category)} />
+      </div>
       <ExpenseList 
-      expenses={expenses} 
+      expenses={visibleExpenses} 
       onDelete={(id) => setExpenses(expenses.filter( e => e.id !== id))} 
     />
     </div>
--- Expenses/src/components/ExpenseFilter.tsx
+++ Expenses/src/components/ExpenseFilter.tsx
@@ -1,15 +1,14 @@
+import categories from '../categories';
 
-interface Props {
-    onSelectCategory: (category: string) => void;
-  }
-  
-const ExpenseFilter = ({onSelectCategory}: Props) => {
+interface Props { 
+  onSelectCategory: (category: string) => void;
+}
+
+const ExpenseFilter = ({ onSelectCategory }: Props) => {
   return (
-    <select className="form-select" onChange={(event)=> onSelectCategory(event.target.value)}>
-        <option value="">All categories</option>
-        <option value="Groceries">Groceries</option>
-        <option value="Utilities">Utilities</option>
-        <option value="Entertainment">Entertainment</option>
+    <select className="form-select" onChange={(event) => onSelectCategory(event.target.value)}>
+      <option value="">All categories</option>
+      {categories.map(category => <option key={category} value={category}>{category}</option>)}
     </select>
   )
 }
